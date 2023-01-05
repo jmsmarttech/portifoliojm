0f08c6e560d6d83555ecd17d7c9b79d7489034fe
Jean Marcelo <108449400+jmsmarttech@users.noreply.github.com>
2023-01-05 14:03:07 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,8 @@
-/* 
-
-    OBJETIVO - quando clicarmos na aba temos que mostrar o conteúdo da aba que foi clicada pelo usuário e esconder o conteúdo da aba anterior
-
-    - passo 1 - dar um jeito de pegar os elementos que representam as abas no HTML
-
-    - passo 2 - dar um jeito de identificar o clique no elemento da aba
-
- 	- passo 3 - quando o usuário clicar, desmarcar a aba selecionada
-
-	- passo 4 - marcar a aba clicada como selecionado
-
-	- passo 5 - esconder o conteúdo anterior
-
-	- passo 6 - mostrar o conteúdo da aba selecionada
-
-*/
-
-// passo 1 - dar um jeito de pegar os elementos que representam as abas no HTML
+// 1 - elementos que representam as abas no HTML
 const abas = document.querySelectorAll(".aba");
 
 abas.forEach(aba => {
-    // passo 2 - dar um jeito de identificar o clique no elemento da aba
+    // 2 - apresentar troca de elementos aba
     aba.addEventListener("click", function() {
 
         if(aba.classList.contains("selecionado")){
